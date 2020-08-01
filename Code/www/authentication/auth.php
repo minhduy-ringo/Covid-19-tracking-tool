eb71f06fc9539c16763b51af4318cce6e684c488
Võ Minh Duy <minhduy.ringo@gmail.com>
2020-08-01 21:00:38 +0700

    + Generate session and save to database when login success

--- Code/www/authentication/auth.php
+++ Code/www/authentication/auth.php
@@ -10,24 +10,34 @@
         
         $username = explode('=', $query[0]);
         $password = explode('=', $query[1]);
-        $ipAdress = $_SERVER['REMOTE_ADDR'];
+        $ipadress = $_SERVER['REMOTE_ADDR'];
 
         $data = [
             "username"  => $username[1],
             "password"  => $password[1],
-            "ipAddress" => $ipAdress
+            "ipaddress" => $ipadress
         ];
 
+        // GET username and user id 
         $response = callApi('GET', 'auth', $data);
-
-        if(isset($response['UserId']))
+        
+        // If returned response array has user id key
+        if($response['code'] == '200' && count($response['data']))
         {
-            regenerateSession($response['UserId'], $username[1]);
+            regenerateSession($response['data']['UserId'], $response['data']['UserName']);
             setcookie('sessionId', session_id(), time() + 60 * 60, "/", "", true, true);
+
+            $session = [
+                'sessionid'         => session_id(),
+                'userid'            => $_SESSION['userId'],
+                'username'          => $_SESSION['userName'],
+                'expirationdate'    => $_SESSION['expire']
+            ];
+            callApi('POST', 'session', $session);
 
             echo "success";
         }
-        else if(isset($response['ERROR']))
+        else if(isset($response['ERROR']))  // else print error code
         {
             echo $response['ERROR'];
         }
