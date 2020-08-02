092d714a4c2072f40d3988e529d59fa21a0c0ed7
Võ Minh Duy <minhduy.ringo@gmail.com>
2020-08-02 19:31:42 +0700

    + Add scrapy to crawl data from Wordometer

---
+++ Code/crawler/run_crawler_auto.bat
@@ -0,0 +1,4 @@
+@echo on
+
+D:\DevTools\Anaconda3\python.exe "D:\Uni\Year 6\Sem 2\UDPT\Covid-19-tracking-tool\Code\crawler\run_crawler.py"
+pause
---
+++ Code/crawler/ultils.py
@@ -0,0 +1,36 @@
+# Support looking for tracking file crawl by corona spider
+
+import glob
+import os
+
+dir_name = os.path.dirname(__file__)
+tracking_dir = os.path.join(dir_name, 'corona_tracking/')
+
+def FindByDate(date):
+    """
+        Find a file in tracking folder by input date and return an absolute path to the file\n
+        FindByDate(date: string)\n
+        Date format: ddMmm Ex: 06Jun
+    """
+    os.chdir(tracking_dir)
+    try:
+        file = glob.glob('*' + date + '*')
+        fpath = tracking_dir + file[0]
+    except IndexError:
+        print("File not found!")
+    return fpath
+
+def AllFiles():
+    """
+        Return a list contains absolute path to all the files in tracking folder\n
+    """
+    os.chdir(tracking_dir)
+    all_files = glob.glob('*.csv')
+    try:
+        fpath = []
+        for file in all_files:
+            file = tracking_dir + file
+            fpath.append(file)
+    except IndexError:
+        print("No tracking file")
+    return fpath
